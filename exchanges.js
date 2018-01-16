9859f34ce9aef21e14aa325bd394eac2d9838182
utkarshv <utkarshvashishtha123@gmail.com>
2018-01-16 19:32:52 +0530

    middlewares and JWT auth passing

--- exchanges.js
+++ exchanges.js
@@ -1,5 +1,5 @@
 console.log("");
-console.log("//************************* Welcome to exchanges **************************//");
+console.log("//************************* Welcome to exchangess **************************//");
 console.log("");
 
 
@@ -10,15 +10,16 @@ console.log("");
     const config = require('./lib/config');
     
 
-    //connect to db
-    config.dbConfig(config.cfg, (err) => {
-        if (err) {
-            console.log(err, 'exiting the app.');
-            return;
-        }
+    // connect to db
+config.dbConfig(config.cfg, (err) => {
+    if (err) {
+        logger.error(err, 'exiting the app.');
+        return;
+    }
 
     // load external modules
     const express = require("express");
+
 
     // init express app
     const app = express();
@@ -28,6 +29,10 @@ console.log("");
 
     // config express
     config.expressConfig(app, config.cfg.environment);
+
+    // attach the routes to the app
+    require("./lib/routes")(app);
+    //require("./lib/post")(app);
 
     // start server
     app.listen(config.cfg.port, () => {
