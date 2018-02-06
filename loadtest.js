329608e6a7d71f8d041ccb1df215a7859467a931
utkarshv <utkarshvashishtha123@gmail.com>
2018-02-06 11:53:21 +0530

    user module apis

--- loadtest.js
+++ loadtest.js
@@ -1,3 +1,3 @@
 
 
-loadtest http://localhost:7357/exchanges/api/v1/exchange/exchange -t 2 -c 5 --rps 500 -P '{"user_id": "5a682ba43fd2fa19ddfe3ded","amount":{"value":0.5,"currency":"BCH"},"volume":{"value":5,"currency":"INR"},"rate":0.1,"type":"BID"}'
+loadtest http://localhost:7357/exchanges/api/v1/exchange/exchange -t 2 -c 5 --rps 10000 -P '{"user_id": "5a682ba43fd2fa19ddfe3ded","amount":{"value":0.5,"currency":"BCH"},"volume":{"value":5,"currency":"INR"},"rate":0.1,"type":"BID"}'
